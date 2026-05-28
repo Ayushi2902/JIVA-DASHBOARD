694a7c6026e6562c28bc08e4dced0c974589fe27
Ayushi Singh <ayushisingh.email@gmail.com>
2026-05-28 14:31:23 +0530

    Delete src directory

--- src/components/common/Avatar.jsx
+++
@@ -1,8 +0,0 @@
-export default function Avatar({ initials, color='bg-emerald-500', size='md', className='' }) {
-  const sizes = { sm:'w-8 h-8 text-xs', md:'w-10 h-10 text-sm', lg:'w-14 h-14 text-base', xl:'w-20 h-20 text-xl' };
-  return (
-    <div className={`${sizes[size]} ${color} rounded-2xl flex items-center justify-center text-white font-bold flex-shrink-0 font-display ${className}`}>
-      {initials}
-    </div>
-  );
-}
